0848eb87457ceefd8398a678691a28e6785241d2
yeomin4242 <whereismysejong@naver.com>
2025-03-12 14:05:16 +0900

    reTest for Horizontal ads

--- src/app/(main)/_components/FoldableHorizontalAdsense.tsx
+++ src/app/(main)/_components/FoldableHorizontalAdsense.tsx
@@ -1,17 +1,45 @@
-import Script from "next/script";
+"use client";
 
-const FoldableHorizontalAdsense = () => {
-  if (process.env.NODE_ENV !== "production") {
-    return null;
-  }
+import { useEffect, useState } from "react";
+
+const FoldableHorizontalAdsense: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true); // 광고 표시 여부
+
+  useEffect(() => {
+    // Initialize adsbygoogle
+    if (window.adsbygoogle) {
+      window.adsbygoogle.push({});
+    }
+  }, []);
+
+  if (!isVisible) return null; // 닫기 버튼 클릭 시 광고 숨김
 
   return (
-    <Script
-      id="adsense"
-      strategy="afterInteractive"
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT}`}
-      crossOrigin="anonymous"
-    />
+    <div className="fixed bottom-0 left-0 z-50 w-full bg-white shadow-md">
+      <div className="relative flex items-center justify-center">
+        <div className="mx-auto w-full">
+          <ins
+            className="adsbygoogle"
+            style={{
+              display: "block",
+              width: "100%",
+              height: "90px",
+              minHeight: "50px",
+            }}
+            data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT}
+            data-ad-slot={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_HORIZON_SLOT}
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          />
+        </div>
+        <button
+          className="absolute right-4 top-1 rounded-md bg-gray-700 px-3 py-1 text-sm text-white hover:bg-gray-900"
+          onClick={() => setIsVisible(false)}
+        >
+          ✖ Close
+        </button>
+      </div>
+    </div>
   );
 };
 
